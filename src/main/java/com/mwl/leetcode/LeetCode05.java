e1e4308e95845e6ed2f930e2044da863acbe5db9
mawenlong <15061886113@163.com>
2018-08-20 21:25:54 +0800

    Longest Palindromic Substring

---
+++ src/main/java/com/mwl/leetcode/LeetCode05.java
@@ -0,0 +1,48 @@
+package com.mwl.leetcode;
+
+/**
+ * @author mawenlong
+ * @date 2018/08/20
+ * describe:
+ * Given a string s, find the longest palindromic substring in s.
+ * You may assume that the maximum length of s is 1000.
+ * <p>
+ * Example 1:
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ */
+
+public class LeetCode05 {
+    /**
+     * 扩展中心法
+     *
+     * @param s
+     * @return java.lang.String
+     */
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int l = left, r = right;
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+}
