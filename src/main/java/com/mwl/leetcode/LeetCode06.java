a2691b72f027b0bb9f2112562c93be99bf86aadb
mawenlong <15061886113@163.com>
2018-09-02 20:41:34 +0800

    leetcode 06

--- src/main/java/com/mwl/leetcode/LeetCode06.java
+++ src/main/java/com/mwl/leetcode/LeetCode06.java
@@ -5,17 +5,17 @@ package com.mwl.leetcode;
  * @date 2018/9/2
  * describe:
  */
-public class Solution {
+public class LeetCode06 {
     public String convert(String s, int numRows) {
         if (numRows == 1) {
             return s;
         }
         int len = s.length();
-        int rowcy = len * 2 - 2;
+        int rowcy = numRows * 2 - 2;
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j + i < len; j += rowcy) {
-                sb.append(s.charAt(i));
+                sb.append(s.charAt(j + i));
                 if (i != 0 && i != numRows - 1 && j + rowcy - i < len) {
                     sb.append(s.charAt(j + rowcy - i));
                 }
