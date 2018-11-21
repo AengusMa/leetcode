7bec3da86feeb94a44cfe7018cf8ac63e144f62a
mawenlong <15061886113@163.com>
2018-11-21 18:45:11 +0800

    leetcode 09

---
+++ src/main/java/com/mwl/leetcode/LeetCode09.java
@@ -0,0 +1,26 @@
+package com.mwl.leetcode;
+
+/**
+ * @author mawenlong
+ * @date 2018/11/20
+ */
+public class LeetCode09 {
+    public boolean isPalindrome(int x) {
+        if (x == 0) {
+            return true;
+        }
+        if (x < 0) {
+            return false;
+        }
+        int tag = 0;
+        int org = x;
+        while (x != 0) {
+            tag = tag * 10 + x % 10;
+            x = x / 10;
+        }
+        if (tag == org) {
+            return true;
+        }
+        return false;
+    }
+}
