9a152f102e34e1fa41fe5451164977d3ed1056b3
mawenlong <mwljndx@163.com>
2019-04-08 21:15:04 +0800

    leetcode

--- go/027/leetcode027.go
+++ go/027/leetcode027.go
@@ -1,8 +1,16 @@
-package leetcode026
+package leetcode027
 
-func removeDuplicates(nums []int) int {
-	for i := 0; i < len(nums); i++ {
-
+func removeElement(nums []int, val int) int {
+	left, right := 0, len(nums)-1
+	valCount := 0
+	for left <= right {
+		if nums[left] == val {
+			nums[left], nums[right] = nums[right], nums[left]
+			right--
+			valCount++
+		} else {
+			left++
+		}
 	}
-	return 0
+	return len(nums) - valCount
 }
--- go/027/leetcode027_test.go
+++ go/027/leetcode027_test.go
@@ -1,7 +1,14 @@
 package leetcode027
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test027(t *testing.T) {
-
+	nums := []int{3, 2, 2, 3}
+	fmt.Println(removeElement(nums, 2))
+	for _, ele := range nums {
+		println(ele)
+	}
 }
