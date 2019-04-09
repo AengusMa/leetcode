aa25d0da41ac483abdc37df48ab34cdb2a723e2d
mawenlong <mwljndx@163.com>
2019-04-09 21:21:11 +0800

    leetcode

--- go/026/leetcode026.go
+++ go/026/leetcode026.go
@@ -1,8 +1,19 @@
 package leetcode026
 
 func removeDuplicates(nums []int) int {
-	for i := 0; i < len(nums); i++ {
-
+	if len(nums) == 0 {
+		return 0
 	}
-	return 0
+	prev := nums[0]
+	slot := 1
+	for i := 1; i < len(nums); i++ {
+		n := nums[i]
+		if n == prev {
+			continue
+		}
+		nums[slot] = n
+		slot++
+		prev = n
+	}
+	return slot
 }
--- go/026/leetcode026_test.go
+++ go/026/leetcode026_test.go
@@ -1,7 +1,14 @@
 package leetcode026
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test026(t *testing.T) {
-
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	fmt.Println(removeDuplicates(nums))
+	for _, ele := range nums {
+		println(ele)
+	}
 }
