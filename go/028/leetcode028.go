b6ab515de468031875ae139d6e0c7d9830c1dbf0
mawenlong <mwljndx@163.com>
2019-04-10 22:58:46 +0800

    leetcode

---
+++ go/028/leetcode028.go
@@ -0,0 +1,14 @@
+package leetcode028
+
+func strStr(haystack string, needle string) int {
+	if haystack == needle {
+		return 0
+	}
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if haystack[i:len(needle)+i] == needle {
+			return i
+		}
+	}
+
+	return -1
+}
---
+++ go/028/leetcode028_test.go
@@ -0,0 +1,10 @@
+package leetcode028
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test028(t *testing.T) {
+	fmt.Println(strStr("aaabbbccc", "bb"))
+}
