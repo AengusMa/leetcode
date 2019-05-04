260467a55105647f134dcfdf5f42481716820598
mawenlong <mwljndx@163.com>
2019-05-04 19:27:49 +0800

    leetcode

---
+++ go/100/leetcode088/leetcode088_test.go
@@ -0,0 +1,14 @@
+package leetcode088
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test088(t *testing.T) {
+
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	merge(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
+}
