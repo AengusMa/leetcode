202378cd912cd4ab44bc0e2dfb4fdd7bdda3a89d
mawenlong <mwljndx@163.com>
2019-05-22 19:53:15 +0800

    leetcode

---
+++ go/150/leetcode101/leetcode101_test.go
@@ -0,0 +1,26 @@
+package leetcode101
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return ish(root.Left, root.Right)
+}
+
+func ish(l, r *TreeNode) bool {
+	if l == nil || r == nil {
+		return l == r
+	}
+
+	if l.Val != r.Val {
+		return false
+	}
+
+	return ish(l.Left, r.Right) && ish(l.Right, r.Left)
+}
