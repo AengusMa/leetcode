c28e0d9af8f263a0ae6fc70f48176d9638ee377d
mawenlong <mwljndx@163.com>
2019-06-01 20:13:29 +0800

    leetcode

---
+++ go/150/leetcode112/leetcode112.go
@@ -0,0 +1,31 @@
+package leetcode111
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	return helper(root, 0, sum)
+}
+
+func helper(node *TreeNode, cur int, dest int) bool {
+	if node.Left != nil {
+		if helper(node.Left, cur+node.Val, dest) {
+			return true
+		}
+	}
+	if node.Right != nil {
+		if helper(node.Right, cur+node.Val, dest) {
+			return true
+		}
+	}
+	if node.Left == nil && node.Right == nil {
+		return (cur+node.Val == dest)
+	}
+	return false
+}
