974efa92f15ece8c4d158b3663dbf519e91e7f90
mawenlong <mwljndx@163.com>
2019-06-13 20:27:35 +0800

    leetcode

---
+++ go/150/leetcode105/leetcode105.go
@@ -0,0 +1,45 @@
+package leetcode105
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(pre []int, in []int) *TreeNode {
+	if len(pre) == 0 {
+		return nil
+	}
+
+	rootVal := pre[0]
+	if len(pre) == 1 {
+		var root TreeNode
+		root.Val = rootVal
+		return &root
+	}
+
+	var leftTree *TreeNode
+	i := 0
+	for ; i < len(in); i++ {
+		if in[i] == rootVal {
+			break
+		}
+	}
+
+	if i != 0 {
+		leftTree = buildTree(pre[1:i+1], in[0:i])
+	}
+
+	var rightTree *TreeNode
+	if i+1 < len(pre) && i+1 < len(in) {
+		rightTree = buildTree(pre[i+1:], in[i+1:])
+	}
+
+	var rootTree TreeNode
+	rootTree.Val = rootVal
+	rootTree.Left = leftTree
+	rootTree.Right = rightTree
+
+	return &rootTree
+
+}
