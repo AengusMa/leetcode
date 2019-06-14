9d14afaaa3a58e4a09a9a1f0f20fc682e1cbffdf
mawenlong <mwljndx@163.com>
2019-06-14 17:42:09 +0800

    leetcode

---
+++ go/150/leetcode106/leetcode106.go
@@ -0,0 +1,33 @@
+package leetcode106
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func Index(xs []int, n int) int {
+	for i, x := range xs {
+		if x == n {
+			return i
+		}
+	}
+	return -1
+}
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	if len(inorder) > 0 && len(postorder) > 0 {
+		node := &TreeNode{Val: postorder[len(postorder)-1]}
+		idx := Index(inorder, node.Val)
+
+		leftInorder := inorder[:idx]
+		leftPostorder := postorder[:len(leftInorder)]
+		node.Left = buildTree(leftInorder, leftPostorder)
+
+		rightInorder := inorder[idx+1:]
+		rightPostorder := postorder[len(leftPostorder) : len(postorder)-1]
+		node.Right = buildTree(rightInorder, rightPostorder)
+
+		return node
+	}
+	return nil
+}
