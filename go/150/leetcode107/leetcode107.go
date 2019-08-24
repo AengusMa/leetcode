fcc4f5293d9d79bd59455b7805d3fa99d960f8ba
mawenlong <mwljndx@163.com>
2019-08-24 20:28:12 +0800

    leetcode

--- go/150/leetcode107/leetcode107.go
+++ go/150/leetcode107/leetcode107.go
@@ -23,15 +23,12 @@ func dfs(root *TreeNode, res [][]int, level int) [][]int {
 	if root == nil {
 		return res
 	}
-
 	if len(res)-1 < level {
 		res = append(res, append(make([]int, 0), root.Val))
 	} else {
 		res[level] = append(res[level], root.Val)
 	}
-
 	level++
-
 	res = dfs(root.Left, res, level)
 	res = dfs(root.Right, res, level)
 	return res
